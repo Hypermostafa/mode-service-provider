ad2f579fa6634703835f20dda71e4614939e6237
mostafa <104630058+Hypermostafa@users.noreply.github.com>
2025-11-12 21:35:51 +0200

    Update model.dart

--- model.dart
+++ model.dart
@@ -9,7 +9,7 @@ class AccountModel {
 
   factory AccountModel.fromJson(Map<String, dynamic> json) {
     return AccountModel(
-      // handle both spellings safely
+  
       message: (json["message"] ?? json["messege"] ?? "No message").toString(),
       status: json["status"] ?? false,
     );
